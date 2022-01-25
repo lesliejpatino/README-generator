83fadb9c8ad70b50e4fe373e20729a16275506ba
Leslie Patino <lesliejpatino@gmail.com>
2022-01-24 16:32:16 -0800

    added badget that will be displayed near the top of the readme.

--- index.js
+++ index.js
@@ -1,64 +1,74 @@
 const fs = require('fs')
 const inquirer = require("inquirer");
 
+// variables to store license information. object data type.
+// MIT 
+// const licenseInfo = {
+//     MIT: "Mit",
+//     ISC: "",
+//     "MPL_2.0": ""
+// }
+
+
+// var updateLicense = function(holder, license){
+
+//     return `Copyright 2022 <${holder}>
+//     ${licenseInfo[license]}
+//     `
+// }
+
 
 inquirer
     .prompt([
         {
             type: "input",
             message: "What is your project title?",
-            answer: "title"
+            name: "title"
 
         },
         {
             type: "input",
             message: "Describe your project:",
-            answer: "description"
+            name: "description"
 
-        },
-        // May remove toc
-        {
-            type: "input",
-            message: "tableofcontents",
-            answer: ""
         },
         {
             type: "input",
             message: "What are the steps required to install your project?",
-            answer: "install"
+            name: "install"
         },
         {
             type: "input",
             message: "Provide instructions and examples for use:",
-            answer: "usage"
+            name: "usage"
         },
         {
             type: "list",
             message: "Which license would you like to use?",
-            answer: "license",
+            name: "license",
             choices : [
-                'MIT License',
-                'ISC License',
-                'Mozilla Public License',
-                'Open Database License',
+                'MIT',
+                'ISC',
+                'MPL_2.0',
             ]
         },
         {
             type: "input",
             message: "Who contributed to this project?",
-            answer: "contributor"
+            name: "contributor"
         },
         {
             type: "input",
             message: "What is your email?",
-            answer: "email"
+            name: "email"
             // this needs to be added under questions.
         },
         {
             type: "input",
             message: "What is your GitHub username?",
-            answer: "github"
+            name: "github"
             // this needs to be added under questions
         },
-
-    ])
+    ]).then(answers => {
+        let readMe = `# ${answers.title}
+![License: ${answers.license}](https://img.shields.io/badge/License-${answers.license}-brightgreen.svg)
